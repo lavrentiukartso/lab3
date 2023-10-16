5294ff7f7c9d88f7f8f3d5a6690f2c5054997f46
lavrentiukartso <lavrentyuk10@gmail>
2023-10-16 14:39:08 +0300

    Solution

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
-
+import math
 
 def quadratic_solver(a, b, c):
-    ...
+    # Обчислює дискримінант
+    discriminant = b**2 - 4*a*c
 
-
-
-
-# Приклад використання функції
-a = 1
-b = -3
-c = 2
-result = quadratic_solver(a, b, c)
-print(result)
+    # Якщо дискримінант менше 0, корені відсутні у множині дійсних чисел
+    if discriminant < 0:
+        return "Корені відсутні в множині дійсних чисел"
+    elif discriminant == 0:
+        # Якщо дискримінант рівний 0, є один корінь
+        root = -b / (2*a)
+        return (root,)
+    else:
+        # Якщо дискримінант більше 0, є два корені
+        root1 = (-b + math.sqrt(discriminant)) / (2*a)
+        root2 = (-b - math.sqrt(discriminant)) / (2*a)
+        return (root1, root2)
